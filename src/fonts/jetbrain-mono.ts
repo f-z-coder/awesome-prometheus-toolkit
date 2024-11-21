8bf6d89428b0963888cebc7be6cbc24938253420
Faiz Shaikh <faizshaikh72740@gmail.com>
2024-11-21 15:03:22 +0530

    intial setup

---
+++ src/fonts/jetbrain-mono.ts
@@ -0,0 +1,7 @@
+import { JetBrains_Mono } from "next/font/google"
+
+export const jetBrainMono = JetBrains_Mono({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-jetbrains-mono",
+})
